4d930f22904fea5200a6f1f15fc04b06261d4016
uiytt <kahaneeden@protonmail.com>
2020-08-22 01:44:42 +0200

    Fixed crash
    Mod crashed with modmenu without clothApi

--- src/main/java/fr/uiytt/fastgrowth/config/ModMenuIntegration.java
+++ src/main/java/fr/uiytt/fastgrowth/config/ModMenuIntegration.java
@@ -3,6 +3,8 @@ package fr.uiytt.fastgrowth.config;
 import fr.uiytt.fastgrowth.FastGrowth;
 import io.github.prospector.modmenu.api.ConfigScreenFactory;
 import io.github.prospector.modmenu.api.ModMenuApi;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.gui.screen.Screen;
 
 public class ModMenuIntegration implements ModMenuApi {
     @Override
@@ -11,7 +13,11 @@ public class ModMenuIntegration implements ModMenuApi {
     }
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return ClothConfigManager::build;
+        if(FabricLoader.getInstance().isModLoaded("cloth-config2")) {
+            return ClothConfigManager::build;
+        } else {
+            return ModMenuApi.super.getModConfigScreenFactory();
+        }
 
     }
 }
