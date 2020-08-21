1ad66fd212223f6bc41b99e4866f67bdfc255f67
uiytt <kahaneeden@protonmail.com>
2020-08-21 22:59:43 +0200

    Updated to 1.16.x

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ FastGrowth is a [FABRIC](https://fabricmc.net/ "FABRIC") mod.
 
 Forge version: https://www.curseforge.com/minecraft/mc-mods/tree-growing-simulator
 
-This mod is currently available for Minecraft 1.15.2
+This mod is currently available for Minecraft 1.15.2 and 1.16.x, you can ask nicely for a port in another version on the github issue 
 
 ### Dependencies
 Apart from fabric
@@ -16,4 +16,4 @@ If you are using ModMenu, you will also need [ClothConfig APIv2](https://www.cur
 #### More infos
 If this mod is on a server, you do NOT need it to connect to this server.
 
-You are totally free of using this mod in any modpack. 
+FastGrowth may be included in mod packs at will. No direct permissions are necessary. A list of mods, their authors, and the permission information must be made available for your pack, howeve
--- src/main/java/fr/uiytt/fastgrowth/config/ClothConfigManager.java
+++ src/main/java/fr/uiytt/fastgrowth/config/ClothConfigManager.java
@@ -5,27 +5,28 @@ import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.TranslatableText;
 
 public class ClothConfigManager {
     public static Screen build(Screen parent) {
         ConfigBuilder builder = ConfigBuilder.create()
                 .setParentScreen(parent)
-                .setTitle("title." + FastGrowth.MODID +".config");
-        ConfigCategory general = builder.getOrCreateCategory("dummyName");
+                .setTitle(new TranslatableText("title." + FastGrowth.MODID +".config"));
+        ConfigCategory general = builder.getOrCreateCategory(new TranslatableText("dummyName"));
 
         ConfigEntryBuilder entryBuilder = builder.entryBuilder();
-        general.addEntry(entryBuilder.startIntField("config."+FastGrowth.MODID + ".chanceOfFertilizing",FastGrowth.CONFIG_MANAGER.getChanceOfFertilizing())
+        general.addEntry(entryBuilder.startIntField(new TranslatableText("config."+FastGrowth.MODID + ".chanceOfFertilizing"),FastGrowth.CONFIG_MANAGER.getChanceOfFertilizing())
                 .setDefaultValue(33)
-                .setTooltip("Chance to make a plant grow when shifting (between 0 and 100).")
+                .setTooltip(new TranslatableText("config."+FastGrowth.MODID + ".chanceOfFertilizing.tooltip"))
                 .setSaveConsumer(newValue -> {
                     FastGrowth.CONFIG_MANAGER.setChanceOfFertilizing(Math.max(0,Math.min(100,newValue)));
                 })
                 .setMax(100)
                 .setMin(0)
                 .build());
-        general.addEntry(entryBuilder.startIntField("config."+FastGrowth.MODID + ".particleCount",FastGrowth.CONFIG_MANAGER.getParticleCount())
+        general.addEntry(entryBuilder.startIntField(new TranslatableText("config."+FastGrowth.MODID + ".particleCount"),FastGrowth.CONFIG_MANAGER.getParticleCount())
                 .setDefaultValue(10)
-                .setTooltip("Number of particle spawned when a plant grows")
+                .setTooltip(new TranslatableText("config."+FastGrowth.MODID + ".particleCount.tooltip"))
                 .setSaveConsumer(newValue -> {
                     FastGrowth.CONFIG_MANAGER.setParticleCount(newValue);
                 })
